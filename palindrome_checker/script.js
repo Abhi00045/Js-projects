ce6c8d3de7079ac5b315a089e9ebe1c1d9c91ee4
Abhi00045 <128639147+Abhi00045@users.noreply.github.com>
2024-07-20 02:08:21 +0530

    hellp

---
+++ palindrome_checker/script.js
@@ -0,0 +1,23 @@
+let input = document.getElementById("palindromeInput").v;
+let form = document.getElementById("palindrome-form");
+let result = document.getElementById("result");
+let submit = document.getElementById("submit");
+
+submit.addEventListener("click",()=>{
+    let inputValue = document.getElementById("palindromeInput").value;
+    if(inputValue===""){
+        alert("enter the value here :smile ");
+    }else{
+        let h1 = document.createElement("h1");
+        result.append(h1);
+        let reversed = inputValue.reverse();
+        if(inputValue===reversed){
+            h1.innerHTML="Palindrome it is :happy";
+        }
+        else{
+            h1.innerHTML="not a palindrome number :sad";
+        }
+        
+    }
+    console.log("pu");
+})
